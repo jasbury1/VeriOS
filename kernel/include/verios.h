eb7b8aca4cb81a84e6b395bfc482177a256948d0
James Asbury <jamesasbury03@gmail.com>
2020-02-23 00:52:58 -0800

    created a function for initializing tcb

---
+++ kernel/include/verios.h
@@ -0,0 +1,12 @@
+#ifndef VERIOS_H
+#define VERIOS_H
+
+/* Standard includes */
+#include <limits.h>
+#include <stdint.h>
+
+/* Standard macros */
+#define OS_TRUE (uint8_t)1;
+#define OS_FALSE (uint8_t)0;
+
+#endif /* VERIOS_H */
--- kernel/task.c
+++ kernel/task.c
@@ -12,6 +12,7 @@
 
 /* OS specific includes */
 #include "include/task.h"
+#include "include/verios.h"
 #include "../cpu/xtensa/portmacro.h"
 
 /**
@@ -22,6 +23,17 @@
  */
 typedef struct OSTaskControlBlock
 {
+    /* Pointer to last element pushed to stack */
+    volatile StackType_t *stack_pointer;
+
+    /* Set to OS_TRUE if the task was statically allocated and doesn't require freeing */
+    uint8_t is_static;
+
+    TaskPrio_t priority;
+    StackType_t stack_start;
+    StackType_t stack_top;
+    int stack_size;
+    char * task_name;
 
 } TCB_t;
 
@@ -42,6 +54,9 @@ typedef struct OSTaskControlBlock
 int OS_task_create(TaskFunc_t task_func, void *task_arg, const char *task_name, 
             TaskPrio_t prio, int stack_size, int msg_queue_size, TCB_t *task_tcb)
 {
+    int i;
+    StackType_t *task_stack = null;
+
     /* Priority 0 is reserved for the Idle task */
     if(prio == (TaskPrio_t)0) {
         /* TODO: Typechecking to see if this is the idle TCB. IF it is, dont error */
@@ -52,13 +67,38 @@ int OS_task_create(TaskFunc_t task_func, void *task_arg, const char *task_name,
     /* TODO: We assume the stack grows downwards only. Option later on? */
 
     /* Allocate Stack first so that TCB does not interact with stack memory */
+	task_stack = ( StackType_t * ) pvPortMallocStackMem( ( ( ( size_t ) usStackDepth ) * sizeof( StackType_t ) ) );
+    if(task_stack == null) {
+        return -1;
+    }
+    for(i = 0; i < stack_size; ++i) {
+        // TODO: clear the stack. set everything to 0.
+    }
 
-
+    task_tcb = ( TCB_t * ) pvPortMallocTcbMem( sizeof( TCB_t ) );
+    if(task_tcb == null){
+        /* Allocating TCB failed. Free the stack and error out */
+        vPortFree(task_stack);
+        return -1;
+    }
+    
+    /* Handle message queue */
+    if(msg_queue_size > 0) {
+        /* TODO */
+    }
+    return 0;
 }
+
 /*
  * The static counterpart to OS_task_create
  */
-void OS_task_create_s();
+int OS_task_create_s();
+
+void _OS_task_init_tcb(TCB *tcb, const char *task_name, TaskPrio_t prio, int stack_size, 
+        uint8_t is_static)
+{
+    tcb->
+}
 
 void OS_task_destroy();
 
